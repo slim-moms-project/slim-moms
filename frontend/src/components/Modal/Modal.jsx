17d7beb1ccf97ec90f54bc7c7e3990a8d39604ac
gnihanyazici <yazicigl@gmail.com>
2026-04-27 21:16:08 +0300

    home loader modal

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -1,10 +1,45 @@
-// import css from './Modal.module.css';
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import styles from './Modal.module.css';
 
-const Modal = () => {
-  return (
-    <div>
-      <p></p>
-    </div>
+const modalRoot = document.getElementById('modal-root') || document.body;
+
+const Modal = ({ children, onClose }) => {
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      document.body.style.overflow = 'auto';
+    };
+  }, [onClose]);
+
+  const handleBackdropClick = (e) => {
+    if (e.currentTarget === e.target) {
+      onClose();
+    }
+  };
+
+  return createPortal(
+    <div className={styles.overlay} onClick={handleBackdropClick}>
+      <div className={styles.modal}>
+        {/* Çarpı (Kapatma) Butonu */}
+        <button type="button" className={styles.closeBtn} onClick={onClose}>
+          &times;
+        </button>
+
+        {children}
+      </div>
+    </div>,
+    modalRoot
   );
 };
 
